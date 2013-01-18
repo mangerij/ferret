76ec4a9c05a09ba5b62b386204131a247042bb28
petejw <petejw@dd8cd9ef-2931-0410-98ca-75ad22d19dd1>
2013-01-18 18:46:07 +0000

    git-svn-id: https://localhost:4443/svn/herd/trunk@15945 dd8cd9ef-2931-0410-98ca-75ad22d19dd1
    Signed-off-by: Dmitry Karpeev <karpeev@mcs.anl.gov>

--- ferret.mk
+++ ferret.mk
@@ -5,7 +5,7 @@ ferret_INCLUDE  := $(foreach i, $(ferret_INC_DIRS), -I$(i))
 
 libmesh_INCLUDE := $(ferret_INCLUDE) $(libmesh_INCLUDE)
 
-ferret_LIB := $(FERRET_DIR)/libferret-$(METHOD)$(libext)
+ferret_LIB := $(FERRET_DIR)/libferret-$(METHOD).la
 
 ferret_APP := $(FERRET_DIR)/ferret-$(METHOD)
 
@@ -20,8 +20,6 @@ ferret_objects += $(patsubst %.c, %.$(obj-suffix), $(ferret_csrcfiles))
 ferret_objects += $(patsubst %.f, %.$(obj-suffix), $(ferret_fsrcfiles))
 ferret_objects += $(patsubst %.f90, %.$(obj-suffix), $(ferret_f90srcfiles))
 
-ferret_app_objects := $(patsubst %.C, %.$(obj-suffix), $(FERRET_DIR)/src/main.C)
-
 # plugin files
 ferret_plugfiles   := $(shell find $(FERRET_DIR)/plugins/ -name *.C 2>/dev/null)
 ferret_cplugfiles  := $(shell find $(FERRET_DIR)/plugins/ -name *.c 2>/dev/null)
@@ -34,30 +32,32 @@ ferret_plugins     += $(patsubst %.c, %-$(METHOD).plugin, $(ferret_cplugfiles))
 ferret_plugins     += $(patsubst %.f, %-$(METHOD).plugin, $(ferret_fplugfiles))
 ferret_plugins     += $(patsubst %.f90, %-$(METHOD).plugin, $(ferret_f90plugfiles))
 
+# ferret main
+ferret_main_src    := $(FERRET_DIR)/src/main.C
+ferret_app_objects := $(patsubst %.C, %.$(obj-suffix), $(ferret_main_src))
+
+# dependency files
+ferret_deps := $(patsubst %.C, %.$(obj-suffix).d, $(ferret_srcfiles)) \
+               $(patsubst %.c, %.$(obj-suffix).d, $(ferret_csrcfiles)) \
+               $(patsubst %.C, %.$(obj-suffix).d, $(ferret_main_src))
+
+# If building shared libs, make the plugins a dependency, otherwise don't.
+ifeq ($(libmesh_shared),yes)
+  ferret_plugin_deps := $(ferret_plugins)
+else
+  ferret_plugin_deps :=
+endif
+
 all:: $(ferret_LIB)
 
-# build rule for lib FERRET
-ifeq ($(enable-shared),yes)
-# Build dynamic library
-$(ferret_LIB): $(ferret_objects) $(ferret_plugins)
+$(ferret_LIB): $(ferret_objects) $(ferret_plugin_deps)
 	@echo "Linking "$@"..."
-	@$(libmesh_CC) $(libmesh_CXXSHAREDFLAG) -o $@ $(ferret_objects) $(libmesh_LDFLAGS)
-else
-# Build static library
-ifeq ($(findstring darwin,$(hostos)),darwin)
-$(ferret_LIB): $(ferret_objects)
-	@echo "Linking "$@"..."
-	@libtool -static -o $@ $(ferret_objects)
-else
-$(ferret_LIB): $(ferret_objects)
-	@echo "Linking "$@"..."
-	@$(AR) rv $@ $(ferret_objects)
-endif
-endif
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+	  $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(ferret_objects) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(EXTERNAL_FLAGS) -rpath $(FERRET_DIR)
+	@$(libmesh_LIBTOOL) --mode=install --quiet install -c $(ferret_LIB) $(FERRET_DIR)
 
 # include FERRET dep files
--include $(FERRET_DIR)/src/*/*.d
-
+-include $(ferret_deps)
 
 # how to build FERRET application
 ifeq ($(APPLICATION_NAME),ferret)
@@ -67,15 +67,15 @@ ferret: $(ferret_APP)
 
 $(ferret_APP): $(moose_LIB) $(elk_MODULES) $(ferret_LIB) $(ferret_app_objects)
 	@echo "Linking "$@"..."
-	@$(libmesh_CXX) $(libmesh_CXXFLAGS) $(ferret_app_objects) -o $@ $(ferret_LIB) $(elk_MODULES) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS)
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+          $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(ferret_app_objects) $(ferret_LIB) $(elk_MODULES) $(moose_LIB) $(libmesh_LIBS) $(libmesh_LDFLAGS) $(ADDITIONAL_LIBS)
 
--include $(FERRET_DIR)/src/*.d
 endif
 
 #
 # Maintenance
 #
-delete_list := $(ferret_APP) $(ferret_LIB)
+delete_list := $(ferret_APP) $(ferret_LIB) $(FERRET_DIR)/libferret-$(METHOD).*
 
 ###############################################################################
 # Additional special case targets should be added here
