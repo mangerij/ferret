6b4cad556d6a6d52cf4fdb6a116870b4e120e100
slauae <slauae@dd8cd9ef-2931-0410-98ca-75ad22d19dd1>
2014-01-14 17:09:52 +0000

    git-svn-id: https://localhost:4443/svn/herd/trunk@23910 dd8cd9ef-2931-0410-98ca-75ad22d19dd1
    Signed-off-by: Dmitry Karpeev <karpeev@mcs.anl.gov>

--- include/timesteppers/PostprocessorAdaptiveDT.h
+++ include/timesteppers/PostprocessorAdaptiveDT.h
@@ -42,7 +42,7 @@ protected:
   bool _on_trial, _pps_good;
   Real _increase_rate, _decrease_rate;
   unsigned int _update_step;
-  const PostprocessorValue & _pps_value;
+  PostprocessorValue& _pps_value;
   //history record of pps
   std::deque<PostprocessorValue> _pps_record;
   std::deque<PostprocessorValue> _pps_diff_record;
