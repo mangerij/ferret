dc8c8db0a00b557752a2c10ac15a7fa50b0a7a9f
Dmitry Karpeev <karpeev@mcs.anl.gov>
2014-05-22 14:53:41 -0500

    MooseVariable number fix.

--- src/kernels/WallEnergyDerivative.C
+++ src/kernels/WallEnergyDerivative.C
@@ -83,7 +83,7 @@ WallEnergyDerivative::computeQpJacobian()
 Real
 WallEnergyDerivative::computeQpOffDiagJacobian(unsigned int jvar)
 {
-  mooseAssert(jvar!=variable().index(),"Something wrong: OffDiag coupled to itself.");
+  mooseAssert(jvar!=variable().number(),"Something wrong: OffDiag coupled to itself.");
   if(jvar==_polar_x_var || jvar==_polar_y_var || jvar==_polar_z_var)
   {
     const unsigned int _jj = (jvar==_polar_x_var)? 0: (jvar==_polar_y_var)? 1 : 2;
