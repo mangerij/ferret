2859b5de83a392920abe36c643da64831c3c1542
John <john@Workstation.(none)>
2015-07-26 20:28:13 -0500

    large job to hunt for c-domains

---
+++ include/bcs/DepolScreenBC.h
@@ -0,0 +1,34 @@
+
+
+#ifndef DEPOLSCREENBC_H
+#define DEPOLSCREENBC_H
+
+#include "NodalNormalBC.h"
+
+class DepolScreenBC;
+
+template<>
+InputParameters validParams<DepolScreenBC>();
+
+/**
+ * Boundary condition of a Dirichlet type but ON the P dot n value.
+ * Sets the value in the node based on the screening from the electrode on the
+ * unscreened depolarization field
+ */
+
+class DepolScreenBC : public NodalNormalBC
+{
+public:
+  DepolScreenBC(const std::string & name, InputParameters parameters);
+
+protected:
+  virtual Real computeQpResidual();
+
+  const VariableValue & _polar_x;
+  const VariableValue & _polar_y;
+  const VariableValue & _polar_z;
+  const Real & _value;
+
+};
+
+#endif /* DEPOLSCREENBC_H */
