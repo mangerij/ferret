35b98d57ae0ed1ac29a8aa1bc73042ebd269fa90
Steven <steven.churchill@uconn.edu>
2017-05-10 22:33:01 -0400

    Added Postprocessors

--- include/postprocessors/BulkEnergyPSTO.h
+++ include/postprocessors/BulkEnergyPSTO.h
@@ -1,16 +1,10 @@
-/**
- * @file   BulkEnergy.C
- * @author S. Churchell <steve.churchill@uconn.edu>
- *
- * @brief
- *  Total free energy postprocessor for PSTO (2D ferroelectric) 
- *
- */
+
 
 
 #ifndef BULKENERGYPSTO_H
 #define BULKENERGYPSTO_H
 
+//TODO: include the base header
 #include "ElementIntegralPostprocessor.h"
 
 //Forward Declarations
@@ -19,6 +13,7 @@ class BulkEnergyPSTO;
 template<>
 InputParameters validParams<BulkEnergyPSTO>();
 
+//TODO: change the base class!
 class BulkEnergyPSTO : public ElementIntegralPostprocessor
 {
 public:
@@ -28,7 +23,8 @@ protected:
   virtual Real computeQpIntegral();
   const VariableValue& _polar_x;
   const VariableValue& _polar_y;
-  const Real _alpha1, _alpha2, _alpha3, _alpha4, _alpha5,_x1, _x2, _x3, _x4, _x5, _x6, _epsilon, _T, _Tc;
+  const VariableValue& _polar_z;
+  const Real _alpha1, _alpha2, _alpha3, _alpha4, _alpha5, _T, _Tc;
 
 };
 
