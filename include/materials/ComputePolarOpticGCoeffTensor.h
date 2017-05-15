d411f8875a90f41f7b58d4bfe08c4e3cc3d05dd6
bingus <john.mangeri@uconn.edu>
2017-05-15 19:53:34 -0400

    add another method to compute the polar-optic tensor ... (experimental)

---
+++ include/materials/ComputePolarOpticGCoeffTensor.h
@@ -0,0 +1,47 @@
+/***************************************************************************/
+/* This file is part of FERRET, an add-on module for MOOSE
+
+/* FERRET is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+/* This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+/* You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For help with FERRET please contact J. Mangeri <john.mangeri@uconn.edu>
+   and be sure to track new changes at bitbucket.org/mesoscience/ferret
+
+/****************************************************************************/
+
+#ifndef COMPUTEPOLAROPTICGCOEFFTENSOR_H
+#define COMPUTEPOLAROPTICGCOEFFTENSOR_H
+
+#include "Material.h"
+#include "RankTwoTensor.h"
+#include "ComputePolarOpticGCoeffTensorBase.h"
+
+/**
+ * ComputePolarOpticGCoeffTensor the base class for computing polar-optic adjustments to B_{ij}
+ */
+class ComputePolarOpticGCoeffTensor : public ComputePolarOpticGCoeffTensorBase
+{
+public:
+  ComputePolarOpticGCoeffTensor(const InputParameters & parameters);
+
+protected:
+  virtual void computeQpPolarOpticGCoeffTensor();
+
+  const VariableValue & _polar_x;
+  const VariableValue & _polar_y;
+  const VariableValue & _polar_z;
+
+  const MaterialProperty<RankFourTensor> & _gcoefficient_tensor;
+};
+
+#endif //COMPUTEPOLAROPTICGCOEFFTENSOR_H
---
+++ src/materials/ComputePolarOpticGCoeffTensorBase.C
@@ -0,0 +1,44 @@
+/***************************************************************************/
+/* This file is part of FERRET, an add-on module for MOOSE
+
+/* FERRET is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+/* This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+/* You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For help with FERRET please contact J. Mangeri <john.mangeri@uconn.edu>
+   and be sure to track new changes at bitbucket.org/mesoscience/ferret
+
+/****************************************************************************/
+
+#include "ComputePolarOpticGCoeffTensorBase.h"
+
+template<>
+InputParameters validParams<ComputePolarOpticGCoeffTensorBase>()
+{
+  InputParameters params = validParams<Material>();
+  params.addParam<std::string>("base_name", "Optional parameter that allows the user to define multiple mechanics material systems on the same block, i.e. for multiple phases");
+  return params;
+}
+
+ComputePolarOpticGCoeffTensorBase::ComputePolarOpticGCoeffTensorBase(const InputParameters & parameters) :
+    Material(parameters),
+   _base_name(isParamValid("base_name") ? getParam<std::string>("base_name") + "_" : "" ),
+   _delta_gPO_tensor_name(_base_name + "delta_gPO_tensor"),
+   _delta_gPO_tensor(declareProperty<RankTwoTensor>("delta_gPO_tensor"))
+{
+}
+
+void
+ComputePolarOpticGCoeffTensorBase::computeQpProperties()
+{
+  computeQpPolarOpticGCoeffTensor();
+}
