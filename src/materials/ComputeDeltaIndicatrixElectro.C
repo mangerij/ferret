41173648cb9b0e3dd8c5247ee1c3e86531158f4b
bingus <john.mangeri@uconn.edu>
2017-10-29 00:34:41 +0200

    Updating legal header with contact info. Closes #99

--- src/materials/ComputeDeltaIndicatrixElectro.C
+++ src/materials/ComputeDeltaIndicatrixElectro.C
@@ -14,7 +14,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-   For help with FERRET please contact J. Mangeri <john.mangeri@uconn.edu>
+   For help with FERRET please contact J. Mangeri <mangeri@fzu.cz>
    and be sure to track new changes at bitbucket.org/mesoscience/ferret
 
 ***************************************************************************/
