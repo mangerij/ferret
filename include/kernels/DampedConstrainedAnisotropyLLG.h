b701a9fb7ea4afd901fbd4e99c203402671861af
bingus <john.mangeri@uconn.edu>
2018-02-22 17:29:02 +0100

    addresses #106... attempt at spherical form. Jacobian probably off.. need to double check

---
+++ include/kernels/DampedConstrainedAnisotropyLLG.h
@@ -0,0 +1,58 @@
+/**
+   This file is part of FERRET, an add-on module for MOOSE
+
+   FERRET is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For help with FERRET please contact J. Mangeri <john.mangeri@uconn.edu>
+   and be sure to track new changes at bitbucket.org/mesoscience/ferret
+
+**/
+
+#ifndef DAMPEDCONSTRAINEDANISOTROPYLLG_H
+#define DAMPEDCONSTRAINEDANISOTROPYLLG_H
+
+#include "Kernel.h"
+
+class DampedConstrainedAnisotropyLLG;
+
+template<>
+InputParameters validParams<DampedConstrainedAnisotropyLLG>();
+
+class  DampedConstrainedAnisotropyLLG: public Kernel
+{
+public:
+
+  DampedConstrainedAnisotropyLLG(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  const unsigned int _component;
+  const unsigned int _azimuth_phi_var;
+  const unsigned int _polar_theta_var;
+  const VariableValue & _azimuth_phi;
+  const VariableValue & _polar_theta;
+  const VariableGradient & _azimuth_phi_grad;
+  const VariableGradient & _polar_theta_grad;
+  const Real _alpha;
+  const Real _K1;
+  const Real _K2;
+  const Real _g0;
+  const Real _Ae;
+  const Real _M;
+
+};
+#endif //DAMPEDCONSTRAINEDANISOTROPYLLG_H
