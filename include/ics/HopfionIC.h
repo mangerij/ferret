f06986eeba3e5c1284187d505f2fc9e73802be41
bingus <john.mangeri@uconn.edu>
2019-09-11 10:42:23 +0200

    add custom initial condition

---
+++ include/ics/HopfionIC.h
@@ -0,0 +1,49 @@
+/**
+   This file is part of FERRET, an add-on module for MOOSE
+
+   FERRET is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For help with FERRET please contact J. Mangeri <mangeri@fzu.cz>
+   and be sure to track new changes at bitbucket.org/mesoscience/ferret
+
+**/
+
+#ifndef HOPFIONIC_H
+#define HOPFIONIC_H
+
+#include "InitialCondition.h"
+
+// Forward Declarations
+class HopfionIC;
+namespace libMesh { class Point; }
+
+template<>
+InputParameters validParams<HopfionIC>();
+
+class HopfionIC : public InitialCondition
+{
+public:
+  HopfionIC(const InputParameters & parameters);
+  virtual Real value(const Point & p);
+
+protected:
+
+private:
+  Real _Lhopf;
+  Real _p0;
+  const unsigned int _component;
+
+};
+
+#endif //HOPFIONIC_H
