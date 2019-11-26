0c83fac7a1bc64543692e45fece0180253db3879
bingus <john.mangeri@uconn.edu>
2019-11-26 16:59:24 +0100

    this is only slight modification of FourierNoise.C in phase field, addresses #22

---
+++ include/functions/SDFourierNoise.h
@@ -0,0 +1,58 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Function.h"
+
+class SDFourierNoise;
+class FEProblemBase;
+
+template <>
+InputParameters validParams<SDFourierNoise>();
+
+/**
+ * Generate noise using random fourier series coefficients
+ */
+class SDFourierNoise : public Function
+{
+public:
+  SDFourierNoise(const InputParameters & parameters);
+
+  virtual Real value(Real, const Point & p) const override;
+
+protected:
+  struct SeriesItem
+  {
+    /// k-vector
+    RealVectorValue k;
+    /// sin coefficient
+    Real s;
+    /// cos coefficient
+    Real c;
+  };
+
+  /// selected lower lengthscale for the noise cut-off
+  const Real _lambda;
+
+  /// Fourier series terms
+  std::vector<SeriesItem> _series;
+
+  /// amplitude factor
+  Real _scale;
+
+  /// actual range
+  Real _range;
+
+  /// actual mid
+  Real _mid;
+
+  /// FEProblem pointer for obtaining the current mesh
+  FEProblemBase & _fe_problem;
+};
