4ea81ef29f6f5d09522ed6c6dc868587ec37f36a
bingus <john.mangeri@uconn.edu>
2020-02-26 16:13:43 +0100

    changes to #12

---
+++ include/kernels/ExchangeCartLLGNorm.h
@@ -0,0 +1,60 @@
+/*
+   This file is part of FERRET, an add-on module for MOOSE
+
+   FERRET is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For help with FERRET please contact J. Mangeri <mangeri@fzu.cz>
+   and be sure to track new changes at github.com/mangerij/ferret
+
+**/
+
+#ifndef EXCHANGECARTLLGNORM_H
+#define EXCHANGECARTLLGNORM_H
+
+#include "Kernel.h"
+
+class ExchangeCartLLGNorm;
+
+template<>
+InputParameters validParams<ExchangeCartLLGNorm>();
+
+class  ExchangeCartLLGNorm: public Kernel
+{
+public:
+  ExchangeCartLLGNorm(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  const unsigned int _component;
+  const unsigned int _mag_x_var;
+  const unsigned int _mag_y_var;
+  const unsigned int _mag_z_var;
+  const VariableValue & _mag_x;
+  const VariableValue & _mag_y;
+  const VariableValue & _mag_z;
+  const VariableGradient & _mag_x_grad;
+  const VariableGradient & _mag_y_grad;
+  const VariableGradient & _mag_z_grad;
+  const Real _alpha;
+  const Real _g0;
+  const Real _Ae;
+  const Real _Ms;
+  const Real _mu0;
+  const VariableValue & _norm;
+
+};
+#endif //EXCHANGECARTLLGNORM_H
