1153c2526b88560f71a10b2da19fb51c41e33b61
bingus <john.mangeri@uconn.edu>
2020-03-06 18:28:39 +0100

    implement some LLB, as opposed to LLCoolJ or LLBean addresses #12

---
+++ include/kernels/LLBLongitudinal.h
@@ -0,0 +1,57 @@
+/*
+   This file is part of FERRET, an add-on module for MOOSE
+
+   FERRET is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For help with FERRET please contact J. Mangeri <mangeri@fzu.cz>
+   and be sure to track new changes at github.com/mangerij/ferret
+
+**/
+
+#ifndef LLBLONGITUDINAL_H
+#define LLBLONGITUDINAL_H
+
+#include "Kernel.h"
+
+class LLBLongitudinal;
+
+template<>
+InputParameters validParams<LLBLongitudinal>();
+
+class  LLBLongitudinal: public Kernel
+{
+public:
+  LLBLongitudinal(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  const unsigned int _component;
+  const unsigned int _mag_x_var;
+  const unsigned int _mag_y_var;
+  const unsigned int _mag_z_var;
+  const VariableValue & _mag_x;
+  const VariableValue & _mag_y;
+  const VariableValue & _mag_z;
+  const Real _alpha;
+  const Real _g0;
+  const Real _Ae;
+  const Real _Ms;
+  const Real _mu0;
+  const Real _alpha_long;
+
+};
+#endif //EXCHANGECARTLLG_H
