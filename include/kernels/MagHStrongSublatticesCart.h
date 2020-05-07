c8d915287b9e38c24335f874dcc198c1d1870826
bingus <john.mangeri@uconn.edu>
2020-05-07 14:33:19 +0200

    add sublattice coupling for LLB problems (antiferromagnets), closes #55

---
+++ include/kernels/MagHStrongSublatticesCart.h
@@ -0,0 +1,66 @@
+/*
+   This file is part of FERRET, an add-on module for MOOSE
+
+   FERRET is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For help with FERRET please contact J. Mangeri <mangeri@fzu.cz>
+   and be sure to track new changes at github.com/mangerij/ferret
+
+**/
+
+#ifndef MAGHSTRONGSUBLATTICESCART_H
+#define MAGHSTRONGSUBLATTICESCART_H
+
+#include "Kernel.h"
+
+class MagHStrongSublatticesCart;
+
+template<>
+InputParameters validParams<MagHStrongSublatticesCart>();
+
+class MagHStrongSublatticesCart: public Kernel
+{
+public:
+
+  MagHStrongSublatticesCart(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+private:
+  const unsigned int _mag1_x_var;
+  const unsigned int _mag1_y_var;
+  const unsigned int _mag1_z_var;
+  const VariableValue & _mag1_x;
+  const VariableValue & _mag1_y;
+  const VariableValue & _mag1_z;
+  const VariableGradient & _mag1_x_grad;
+  const VariableGradient & _mag1_y_grad;
+  const VariableGradient & _mag1_z_grad;
+  const unsigned int _mag2_x_var;
+  const unsigned int _mag2_y_var;
+  const unsigned int _mag2_z_var;
+  const VariableValue & _mag2_x;
+  const VariableValue & _mag2_y;
+  const VariableValue & _mag2_z;
+  const VariableGradient & _mag2_x_grad;
+  const VariableGradient & _mag2_y_grad;
+  const VariableGradient & _mag2_z_grad;
+  const MaterialProperty<Real> & _mu0;
+  const MaterialProperty<Real> & _Ms;
+
+};
+#endif //MAGHSTRONGSUBLATTICESCART_H
