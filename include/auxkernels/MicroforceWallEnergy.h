bf562c9e69975f404a119b3e7bcc189d8d309c8d
John Michael MANGERI <mangeri@mrt-jma-31083.private.list.lu>
2021-02-10 09:36:17 +0100

    closes #90 for now. Bulk and electrostrictive are done, these are most important. Other two are just WIP placeholders for later.

---
+++ include/auxkernels/MicroforceWallEnergy.h
@@ -0,0 +1,51 @@
+/*
+   This file is part of FERRET, an add-on module for MOOSE
+
+   FERRET is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   For help with FERRET please contact J. Mangeri <john.mangeri@list.lu>
+   and be sure to track new changes at github.com/mangerij/ferret
+
+**/
+
+#ifndef MICROFORCEWALLENERGY_H
+#define MICROFORCEWALLENERGY_H
+
+#include "AuxKernel.h"
+
+class MicroforceWallEnergy;
+
+template<>
+InputParameters validParams<MicroforceWallEnergy>();
+
+class MicroforceWallEnergy : public AuxKernel
+{
+public:
+  MicroforceWallEnergy(const InputParameters & parameters);
+
+protected:
+  virtual Real computeValue();
+  const unsigned int _component;
+  const VariableGradient & _polar_i_grad;
+  const VariableGradient & _polar_j_grad;
+  const VariableGradient & _polar_k_grad;
+  const unsigned int _ii, _jj, _kk;
+  const MaterialProperty<Real> & _G110;
+  const MaterialProperty<Real> & _G11;
+  const MaterialProperty<Real> & _G12;
+  const MaterialProperty<Real> & _G44;
+  const MaterialProperty<Real> & _G44P;
+};
+
+#endif // MICROFORCEWALLENERGY_H
