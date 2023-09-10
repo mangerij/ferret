1eca3e604a2198fa5bc75217309533c49f46116e
root <root@laptop1>
2023-09-10 16:08:05 +0200

    update contact info in .C and .h files, addresses changes to the website #252

--- src/functions/SDFourierNoise.C
+++ src/functions/SDFourierNoise.C
@@ -14,7 +14,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-   For help with FERRET please contact J. Mangeri <john.m.mangeri@gmail.com>
+   For help with FERRET please contact J. Mangeri <johnma@dtu.dk>
    and be sure to track new changes at github.com/mangerij/ferret
 
    //adapted from FourierNoise.C [credit to D. Schwen (INL)]
